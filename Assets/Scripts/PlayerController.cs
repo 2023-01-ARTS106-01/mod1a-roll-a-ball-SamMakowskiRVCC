73d7b4054103611323bbb595accf0a6a48c97813
SamMakowskiRVCC <113396202+SamMakowskiRVCC@users.noreply.github.com>
2023-01-25 12:44:26 -0500

    added Spain without the a

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unityengine.InputSystem;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+
     private Rigidbody rb;
     private float movementX;
     private float movementY;
@@ -24,7 +26,7 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vecotr3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement);
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
     }
 }
